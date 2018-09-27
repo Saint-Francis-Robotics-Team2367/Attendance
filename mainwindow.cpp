1de4647831285c764d926f5cc6adb369320fd80b
RohitM2019 <rohitmittal@sfhs.com>
2018-09-27 11:42:26 -0700

    Removed Easter Egg and generlized some dialog

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -40,9 +40,6 @@ int MainWindow::gotText()
     QString time = QTime::currentTime().toString();
 //    QString date = QDate::currentDate().toString();
 
-    if(currStudent->getName()=="Daniel Grau")
-        QSound::play("Yeah.wav");
-
     if (!currStudent->getStatus())
     {   //if the user is not signed in
         
@@ -67,7 +64,7 @@ int MainWindow::gotText()
         }
 
         else {
-            ui->log->append("FREAKING A");
+            ui->log->append("Error in opening data.csv!");
         }
         
     }
@@ -259,10 +256,10 @@ void MainWindow::addNewUser()
     else if (checkExistingUser(name))   {
         QSound::play("error.wav");
         QMessageBox msgBox;
-        msgBox.setText("You are already part of the system. If you entered your ID or Barcode incorrectly, please talk to Daniel Grau or Sameer Vijay");
+        msgBox.setText("You are already part of the system. If you entered your ID or Barcode incorrectly, please talk to a board member or moderator");
         msgBox.setIcon(QMessageBox::Critical);
         msgBox.exec();
-        ui->log->append("You are already part of the system. If you entered your ID or Barcode incorrectly, please talk to Daniel Grau or Sameer Vijay");
+        ui->log->append("You are already part of the system. If you entered your ID or Barcode incorrectly, please talk to a board member or moderator");
     }
 
     else    {
